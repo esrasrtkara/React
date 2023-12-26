6ad63bda6d1cdaa06f121f2c813d54e935a9ed19
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-12-26 16:36:03 +0300

    'async'

--- react-starter/src/pages/Homepage/Homepage.jsx
+++ react-starter/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, json } from 'react-router-dom';
 
 export default function Homepage() {
+    const myAsyncFunction = () =>{
+        return new Promise((resolve,reject) => {
+            //async işlem
+            setTimeout(() => {
+                resolve("veri örneği"); //İşlemin başarılı olması
+                //reject() //işlemin başarısız olması
+                
+            }, 3000);
+        });
+    };
+    useEffect(() => {
+
+      //thenCathFinally();
+      asyncAwait();
+    }, []);
+
+    const asyncAwait = async() =>{
+        try {
+            let response = await myAsyncFunction();
+            console.log(response);
+            
+        } catch (error) {
+            console.log(error);
+        }   
+    }
+
+    const thenCathFinally = () =>{
+        myAsyncFunction().then(response => {
+            console.log("Bir cevap geldi",response);
+          })
+          .catch(err => {
+            console.log("Bir hata geldi",err);
+          })
+          .finally(() => {
+            console.log("Async işlem sonlandı");
+          });
+
+    }
+    
   
   return (
     <div>
