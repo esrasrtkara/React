d11bff2db8716fefb8da2953acfc47747e450dbe
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2023-12-26 17:13:44 +0300

    axios

--- react-starter/src/pages/Homepage/Homepage.jsx
+++ react-starter/src/pages/Homepage/Homepage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect } from 'react'
 import { Link, json } from 'react-router-dom';
 
@@ -5,15 +6,21 @@ export default function Homepage() {
  //http isteği
  useEffect(() => {
     //thenCatchFech();
-    awaitFetch();
+    //awaitFetch();
+    axiosGet();
 
  }, [])
 
- const awaitFetch =async() => {
+  const  axiosGet = async () =>{
+    let response = await axios.get('https://dummyjson.com/products')
+    console.log(response.data);
+  }
+
+  const awaitFetch =async() => {
     let response = await fetch('https://dummyjson.com/products');
     let json = await response.json();
     console.log(json);
- }
+  }
 
   const thenCatchFech = () => {
     fetch('https://dummyjson.com/products')
