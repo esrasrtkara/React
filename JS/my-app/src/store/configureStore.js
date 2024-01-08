b18daee67ef705ba54efb1c0f52ca0220769f10a
Esra AKBULUT <56135111+esrasrtkara@users.noreply.github.com>
2024-01-08 12:00:53 +0300

    -

--- JS/my-app/src/store/configureStore.js
+++ JS/my-app/src/store/configureStore.js
@@ -1,16 +1,10 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
-import {thunk} from "redux-thunk"; // Örnek olarak Redux Thunk middleware kullanımı
 
 export function configureStore() {
-  const middlewares = [thunk]; // Kullanılacak middleware'leri buraya ekleyebilirsiniz
-
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(...middlewares))
+    rootReducer
   );
 
   return store;
